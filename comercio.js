22160f3e39e65f933a3c291842acdd993636395e
Aldair Meraz <aldairrdz@gmail.com>
2022-01-21 09:17:10 -0600

    first commit

---
+++ comercio.js
@@ -0,0 +1,34 @@
+/*
+Definan 2 objetos para modelar el proceso de compra de una plataforma de comercio electrónico:
+
+Artículo: representa el producto que se está comprando y tiene los siguientes atributos:
+nombre
+descripción
+precio
+cantidad
+fotografía URL
+
+Venta: representa una instancia de venta y tiene los siguientes atributos
+artículos (lista de artículos)
+total
+ID del cliente
+correo electrónico del cliente
+
+Implementen los objetos Artículo y Venta usando JavaScript.
+*/
+
+let productos = {
+    producto1: {
+        nombre:"chachitos",
+        descripcion:"cereal a base de trigo muy delicioso",
+        precio:15,
+        cantidad:250,
+        foto:"https://tenor.com/es/ver/john-cena-chachitos-cereal-gif-19932615"
+    },
+    producto2: {
+
+    },
+    producto3: {
+        
+    }
+}
