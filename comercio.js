223cbfa5c49ef955737b91fd65952d03a89bfe66
Aldair Meraz <aldairrdz@gmail.com>
2022-01-21 10:09:04 -0600

    update DOM

--- comercio.js
+++ comercio.js
@@ -26,9 +26,31 @@ let productos = {
         foto:"https://tenor.com/es/ver/john-cena-chachitos-cereal-gif-19932615"
     },
     producto2: {
-
+        nombre:"nutrileche",
+        descripcion:"leche muy deliciosa",
+        precio:10,
+        cantidad:1,
+        foto:"https://i.redd.it/ctfhavulsx231.jpg"
     },
     producto3: {
-        
+        nombre:"ChocoTavo de Fresa",
+        descripcion:"Chocomilk muy deli",
+        precio:18.50,
+        cantidad:500,
+        foto:"https://http2.mlstatic.com/D_NQ_NP_875348-MLM43826141959_102020-V.jpg"
     }
 }
+
+let venta={
+    articulos: function(){
+        return (productos.producto1.nombre+"\n"+productos.producto2.nombre +"\n"+productos.producto3.nombre);
+    },
+    total: function (){
+        return productos.producto1.precio + productos.producto2.precio + productos.producto3.precio;
+    },
+    id: 1234456,
+    email: "atlas_hokage97@gmail.com"
+}
+
+document.getElementById("articulos").innerHTML = venta.articulos();
+document.getElementById("totalArticulos").innerHTML = venta.total();
